a12bdd4242a38064f4597e21928b1918f1493dbd
Peter Huber <peter.huber@brandnamic.com>
2024-11-22 23:24:28 +0100

    Add Dark Mode to CRM Card

--- resources/js/Layouts/CrmCardLayout.vue
+++ resources/js/Layouts/CrmCardLayout.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="h-screen w-screen overflow-hidden">
+    <div class="h-screen w-screen overflow-hidden bg-gray-100 dark:bg-gray-950">
         <slot />
     </div>
 </template>
